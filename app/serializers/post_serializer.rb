259b3249189dc778f842add04a9dfd6e9b1fc25b
akshay <chavanakshaykumar10@gmail.com>
2023-04-23 21:59:50 +0530

    fifth

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -2,3 +2,6 @@ class PostSerializer < ActiveModel::Serializer
   has_many :comments , serializer: CommentSerializer
   attributes :id , :name , :title , :desc 
 end
+
+
+now i will add and remove this changes 
