433b507fb27e3ef275f430fcdf74d2843e9ae930
akshay <chavanakshaykumar10@gmail.com>
2023-04-23 22:59:25 +0530

    one commit

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -10,3 +10,13 @@ this is changes in another branch
 changes for two commit remo
 now i will add and remove this changes 
 
+changes for two commit remove
+
+
+
+last changes
+okay this is finally last
+
+change in later
+
+wsedfhggit  jkdk.jdj.
